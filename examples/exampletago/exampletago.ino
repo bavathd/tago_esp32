dd21ada0c829ef0780ac101a568a06a09785f703
Prasannaa Kumar <d.prasannaakumar@gmail.com>
2024-04-09 11:41:58 +0530

    Update exampletago.ino

--- examples/exampletago/exampletago.ino
+++ examples/exampletago/exampletago.ino
@@ -1,9 +1,9 @@
 #include<tago.h>
 
-tago tag("xyz-wifi", "tha-pass", "YOUR_API_KEY"); //enter your ssid, password and your tago apikey
+tago tag("***ssid_here***", "***password_here***", "YOUR_API_KEY"); //enter your ssid, password and your tago apikey
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   if(!tag.connectWifi()) //To initialize and connect with the WiFi
     {
 
@@ -16,13 +16,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  float co2 = random(0,9) * 10;
-  float rh =  random(0,10) * 0.01;
-  int temp = random(0,100) * 100;
+  float value1 = random(0,9) * 10;
+  float value2 =  random(0,10) * 0.01;
+  int value3 = random(0,100) * 100;
  
-  tag.sendData("Co2", String(co2));   // To send to tago cloud
-  tag.sendData("rh", String(rh));
-  tag.sendData("temp", String(temp));
+  tag.sendData("value1", String(value1));   // To send to tago cloud
+  tag.sendData("value2", String(value2));
+  tag.sendData("value3", String(value3));
   delay(10000);
 
 }
